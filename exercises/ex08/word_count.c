55ad9adab7ee75380d9b5f85c4a36f8b0e180781
yjiang0929 <yichenjiang0929@gmail.com>
2020-04-07 01:16:17 -0400

    add doc strings

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -5,6 +5,13 @@
 
 #define BUFFER_MAX_SIZE 1000
 
+/*
+  Create hashtable of word freaquency from text file. The words will be
+  stripped off punctuations specified in delim. 
+
+  Parameters: table: pointer to Glib hashtable that stores word frequency
+              filename: char string that represents file name to read from
+*/
 void create_hashtable_from_file(GHashTable **table, char *filename) {
     FILE *text;
     gchar word[BUFFER_MAX_SIZE];
@@ -30,10 +37,22 @@ void create_hashtable_from_file(GHashTable **table, char *filename) {
     fclose(text);
 }
 
+/*
+  Print out frequency of each word on stdout
+
+  Parameters: key: word that appears in the text file
+              value: number of times that the word appears
+              user_data: unused data field for GHFunc declaration
+*/
 void print_word_count(gpointer key, gpointer value, gpointer user_data) {
     printf("%s: %d\n", (char *)key, GPOINTER_TO_INT(value));
 }
 
+/*
+  Main body of the program. 
+  Creates a hashtable from input text, and then displays the word count from
+  hashtable on stdout.
+*/
 int main() {
     GHashTable *table = g_hash_table_new(g_str_hash, g_str_equal);
     char *filename = "alice_in_wonderland.txt";
